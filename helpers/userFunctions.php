66a691f96c14da95c8cb3364f6b24abb7dab3eeb
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-10 23:17:11 +0100

    Переписывает функцию обрезки текста и выносит верстку в шаблон

---
+++ helpers/userFunctions.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @param string $text
+ * @param int    $length
+ *
+ * @return string
+ */
+function trimText(string $text, int $length = 300): string
+{
+    if (mb_strlen($text) <= $length) {
+        return $text;
+    }
+
+    $words = explode(' ', $text);
+    $result = [];
+
+    foreach ($words as $word) {
+        if ((mb_strlen(implode(' ', $result)) + mb_strlen($word) + 1) > $length) {
+            break;
+        }
+        $result[] = $word;
+    }
+
+    return implode(' ', $result);
+}
+
+
