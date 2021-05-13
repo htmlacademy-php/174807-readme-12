9a400967e867df8958619dc4958bd3db340d5037
Pavel Klochkov <paul.klochkov@gmail.com>
2021-05-13 10:55:23 +0200

    Выводит список постов из базы данных

--- index.php
+++ index.php
@@ -3,49 +3,80 @@ declare(strict_types=1);
 
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'helpers.php';
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'user-functions.php');
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'db.php');
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'init.php');
+
+$posts = [];
+$content = '';
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+} else {
+    $sql = "SELECT
+                p.title,
+                u.login as username,
+                c.type,
+                c.icon,
+                p.views,
+                p.content,
+                u.avatar as avatar_url
+            FROM readme.posts p
+                INNER JOIN readme.users u ON p.user_id = u.id
+                INNER JOIN readme.content_type c ON p.content_type_id = c.id
+            ORDER BY p.views DESC";
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($link);
+        $content = include_template('error.php', ['error' => $error]);
+    }
+}
 
 $pageTitle = 'readme: популярное';
-$posts = [
-    [
-        'title' => 'Цитата',
-        'type' => 'post-quote',
-        'description' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-        'username' => 'Лариса',
-        'avatar-url' => 'userpic-larisa-small.jpg'
-    ],
-    [
-        'title' => 'Игра престолов',
-        'type' => 'post-text',
-        'description' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-        'username' => 'Владик',
-        'avatar-url' => 'userpic.jpg'
-    ],
-    [
-        'title' => 'Наконец, обработал фотки!',
-        'type' => 'post-photo',
-        'description' => 'rock-medium.jpg',
-        'username' => 'Виктор',
-        'avatar-url' => 'userpic-mark.jpg'
-    ],
-    [
-        'title' => 'Моя мечта',
-        'type' => 'post-photo',
-        'description' => 'coast-medium.jpg',
-        'username' => 'Лариса',
-        'avatar-url' => 'userpic-larisa-small.jpg'
-    ],
-    [
-        'title' => 'Лучшие курсы',
-        'type' => 'post-link',
-        'description' => 'www.htmlacademy.ru',
-        'username' => 'Владик',
-        'avatar-url' => 'userpic.jpg'
-    ]
-];
+//$posts = [
+//    [
+//        'title' => 'Цитата',
+//        'type' => 'post-quote',
+//        'description' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+//        'username' => 'Лариса',
+//        'avatar-url' => 'userpic-larisa-small.jpg'
+//    ],
+//    [
+//        'title' => 'Игра престолов',
+//        'type' => 'post-text',
+//        'description' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+//        'username' => 'Владик',
+//        'avatar-url' => 'userpic.jpg'
+//    ],
+//    [
+//        'title' => 'Наконец, обработал фотки!',
+//        'type' => 'post-photo',
+//        'description' => 'rock-medium.jpg',
+//        'username' => 'Виктор',
+//        'avatar-url' => 'userpic-mark.jpg'
+//    ],
+//    [
+//        'title' => 'Моя мечта',
+//        'type' => 'post-photo',
+//        'description' => 'coast-medium.jpg',
+//        'username' => 'Лариса',
+//        'avatar-url' => 'userpic-larisa-small.jpg'
+//    ],
+//    [
+//        'title' => 'Лучшие курсы',
+//        'type' => 'post-link',
+//        'description' => 'www.htmlacademy.ru',
+//        'username' => 'Владик',
+//        'avatar-url' => 'userpic.jpg'
+//    ]
+//];
 $isAuth = rand(0, 1);
 $userName = 'Павел';
 $maxTextLength = 300;
-$content = include_template( 'main.php', [
+$content = include_template('main.php', [
     'posts' => $posts,
     'maxTextLength' => $maxTextLength
 ]);
--- templates/main.php
+++ templates/main.php
@@ -87,31 +87,31 @@
         <div class="popular__posts">
             <?php foreach ($posts as $index => $post):
                 $postTitle = htmlspecialchars($post['title']);
-                $postDescription = htmlspecialchars($post['description']);
+                $postContent = htmlspecialchars($post['content']);
                 $postDate = generate_random_date($index);
                 $dateTitle = date('d.m.Y H:i', strtotime($postDate));
                 $dateDifferenceText = getDateDifferenceText($postDate); ?>
 
-                <article class="popular__post post <?= $post['type']; ?>">
+                <article class="popular__post post post-<?= $post['icon']; ?>">
                     <header class="post__header">
                         <h2><a href="#"><?= $postTitle; ?></a></h2>
                     </header>
                     <div class="post__main">
-                        <?php if ($post['type'] === 'post-quote'): ?>
+                        <?php if ($post['type'] === 'Цитата'): ?>
                             <blockquote>
                                 <p>
-                                    <?= $postDescription; ?>
+                                    <?= $postContent; ?>
                                 </p>
                                 <cite>Неизвестный Автор</cite>
                             </blockquote>
-                        <?php elseif ($post['type'] === 'post-text'): ?>
-                            <?php if (mb_strlen($post['description']) <= $maxTextLength) { ?>
-                                <p><?= $postDescription; ?></p>
+                        <?php elseif ($post['type'] === 'Текст'): ?>
+                            <?php if (mb_strlen($post['content']) <= $maxTextLength) { ?>
+                                <p><?= $postContent; ?></p>
                             <?php } else { ?>
-                                <p><?= htmlspecialchars(trimText($post['description'], $maxTextLength)) . '...'; ?></p>
+                                <p><?= htmlspecialchars(trimText($post['content'], $maxTextLength)) . '...'; ?></p>
                                 <a class="post-text__more-link" href="#">Читать далее</a>
                             <?php }; ?>
-                        <?php elseif ($post['type'] === 'post-link'): ?>
+                        <?php elseif ($post['type'] === 'Ссылка'): ?>
                             <div class="post-link__wrapper">
                                 <a class="post-link__external" href="http://" title="Перейти по ссылке">
                                     <div class="post-link__info-wrapper">
@@ -122,19 +122,19 @@
                                             <h3><?= $postTitle; ?></h3>
                                         </div>
                                     </div>
-                                    <span><?= $postDescription; ?></span>
+                                    <span><?= $postContent; ?></span>
                                 </a>
                             </div>
 
-                        <?php elseif ($post['type'] === 'post-photo'): ?>
+                        <?php elseif ($post['type'] === 'Картинка'): ?>
                             <div class="post-photo__image-wrapper">
-                                <img src="img/<?= $postDescription; ?>" alt="Фото от пользователя" width="360" height="240">
+                                <img src="img/<?= $postContent; ?>" alt="Фото от пользователя" width="360" height="240">
                             </div>
 
-                        <?php elseif ($post['type'] === 'post-video'): ?>
+                        <?php elseif ($post['type'] === 'Видео'): ?>
                             <div class="post-video__block">
                                 <div class="post-video__preview">
-                                    <?= embed_youtube_cover($postDescription); ?>
+                                    <?= embed_youtube_cover($postContent); ?>
                                     <img src="img/coast-medium.jpg"  alt="Превью к видео" width="360" height="188">
                                 </div>
                                 <a href="post-details.html" class="post-video__play-big button">
@@ -150,7 +150,7 @@
                         <div class="post__author">
                             <a class="post__author-link" href="#" title="Автор">
                                 <div class="post__avatar-wrapper">
-                                    <img class="post__author-avatar" src="img/<?= $post['avatar-url']; ?>" alt="Аватар пользователя <?= $post['username']; ?>">
+                                    <img class="post__author-avatar" src="img/<?= $post['avatar_url']; ?>" alt="Аватар пользователя <?= $post['username']; ?>">
                                 </div>
                                 <div class="post__info">
                                     <b class="post__author-name"><?= $post['username']; ?></b>
