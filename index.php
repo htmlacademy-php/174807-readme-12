fec49aecb537fbf0f02c9a02447accd5df70ad67
Pavel Klochkov <paul.klochkov@gmail.com>
2021-05-23 14:33:18 +0200

    Выводит категории из базы данных

--- index.php
+++ index.php
@@ -8,84 +8,46 @@ require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPAR
 
 $posts = [];
 $content = '';
-
-if (!$link) {
-    $error = mysqli_connect_error();
-    $content = include_template('error.php', ['error' => $error]);
-} else {
-    $sql = "SELECT
-                p.title,
-                u.login as username,
-                c.type,
-                c.icon,
-                p.views,
-                p.content,
-                u.avatar as avatar_url
-            FROM readme.posts p
-                INNER JOIN readme.users u ON p.user_id = u.id
-                INNER JOIN readme.content_type c ON p.content_type_id = c.id
-            ORDER BY p.views DESC";
-    $result = mysqli_query($link, $sql);
-
-    if ($result) {
-        $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    } else {
-        $error = mysqli_error($link);
-        $content = include_template('error.php', ['error' => $error]);
-    }
-}
-
 $pageTitle = 'readme: популярное';
-//$posts = [
-//    [
-//        'title' => 'Цитата',
-//        'type' => 'post-quote',
-//        'description' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-//        'username' => 'Лариса',
-//        'avatar-url' => 'userpic-larisa-small.jpg'
-//    ],
-//    [
-//        'title' => 'Игра престолов',
-//        'type' => 'post-text',
-//        'description' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-//        'username' => 'Владик',
-//        'avatar-url' => 'userpic.jpg'
-//    ],
-//    [
-//        'title' => 'Наконец, обработал фотки!',
-//        'type' => 'post-photo',
-//        'description' => 'rock-medium.jpg',
-//        'username' => 'Виктор',
-//        'avatar-url' => 'userpic-mark.jpg'
-//    ],
-//    [
-//        'title' => 'Моя мечта',
-//        'type' => 'post-photo',
-//        'description' => 'coast-medium.jpg',
-//        'username' => 'Лариса',
-//        'avatar-url' => 'userpic-larisa-small.jpg'
-//    ],
-//    [
-//        'title' => 'Лучшие курсы',
-//        'type' => 'post-link',
-//        'description' => 'www.htmlacademy.ru',
-//        'username' => 'Владик',
-//        'avatar-url' => 'userpic.jpg'
-//    ]
-//];
 $isAuth = rand(0, 1);
 $userName = 'Павел';
 $maxTextLength = 300;
-$content = include_template('main.php', [
-    'posts' => $posts,
-    'maxTextLength' => $maxTextLength
-]);
+
+$sqlContentTypes = "SELECT type, icon FROM readme.content_type";
+$resultContentTypes = mysqli_query($link, $sqlContentTypes);
+$sqlPopularPosts = "SELECT
+                        p.title,
+                        u.login as username,
+                        c.type,
+                        c.icon,
+                        p.views,
+                        p.content,
+                        u.avatar as avatar_url
+                    FROM readme.posts p
+                        INNER JOIN readme.users u ON p.user_id = u.id
+                        INNER JOIN readme.content_type c ON p.content_type_id = c.id
+                    ORDER BY p.views DESC
+                    LIMIT 6;";
+$resultPopularPosts = mysqli_query($link, $sqlPopularPosts);
+$contentTypes = mysqli_fetch_all($resultContentTypes, MYSQLI_ASSOC);
+$popularPosts = mysqli_fetch_all($resultPopularPosts, MYSQLI_ASSOC);
+
+if (!$contentTypes || !$popularPosts) {
+    $error = mysqli_error($link);
+    $content = include_template('error.php', ['error' => $error]);
+} else {
+    $content = include_template('main.php', [
+        'posts' => $popularPosts,
+        'categories' => $contentTypes,
+        'maxTextLength' => $maxTextLength
+    ]);
+}
 
 print include_template('layout.php', [
     'pageTitle' => $pageTitle,
     'isAuth' => $isAuth,
     'userName' => $userName,
     'maxTextLength' => $maxTextLength,
-    'posts' => $posts,
+    'posts' => $popularPosts,
     'content' => $content
 ]);
--- utils/init.php
+++ utils/init.php
@@ -1,4 +1,9 @@
 <?php
 $link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
 
-mysqli_set_charset($link, "utf8");
+if (!$link) {
+    $error = mysqli_connect_error();
+    $main = include_template('error.php', ['error' => $error]);
+} else {
+    mysqli_set_charset($link, "utf8");
+}
